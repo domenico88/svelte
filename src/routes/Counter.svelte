08868dc477e1f2cfaa970f9206d69d155fcf5bd3
domenico88 <domenicosgueglia.88@gmail.com>
2023-03-01 23:51:23 +0100

    wip

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-  let count = 0;
+  import { count } from "../store";
 
-  $: testString = count == 2 ? "Wow" : "";
-  const incrementCount = () => count++;
-  const decreaseCount = () => count--;
+  let countValue: number;
+
+  count.subscribe((value) => {
+    countValue = value;
+  });
+
+  $: testString = countValue == 2 ? "Wow" : "";
+  const incrementCount = () => count.update((c) => c + 1);
+  const decreaseCount = () => count.update((c) => c - 1);
   let buttonStyle =
     "bg-fuchsia-800 hover:bg-fuchsia-800 text-white font-bold py-2 px-4 rounded mr-4";
 </script>
 
 <div class="text-center mt-10">
   <button class={buttonStyle} on:click={incrementCount}>+ </button>
-  {count}
+  {countValue}
   <button class={buttonStyle + " ml-4"} on:click={decreaseCount}>- </button>
   Clicked {testString}
 </div>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,6 +1,47 @@
+<script lang="ts">
+  import { count } from "../../store";
+
+  let countValue: number;
+
+  count.subscribe((value) => {
+    countValue = value;
+  });
+</script>
+
 <svelte:head>
-  <title>About</title>
+  <title>Products</title>
   <meta name="description" content="About this app" />
 </svelte:head>
-
-<div class="text-column" />
+<div class="text-column">
+  {#each Array(countValue) as _, i}
+    <div class="max-w-sm rounded overflow-hidden shadow-lg inline-block mr-5 mt-7">
+      <img
+        class="w-full"
+        src="/img/card-top.jpg"
+        alt="Sunset in the mountains"
+      />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <p class="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#photography</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#travel</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#winter</span
+        >
+      </div>
+    </div>
+  {/each}
+</div>
